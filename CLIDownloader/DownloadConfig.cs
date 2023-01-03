b1d3fbc18a503456485b9aac436f6098d95ef21c
Hamza Ouassil <ouassil@live.com>
2023-01-03 18:58:16 +0100

    Completed “download” command set up. Added “validate” command set up. Parsed YAML config file.

---
+++ CLIDownloader/DownloadConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using YamlDotNet.Serialization;
+
+namespace CLIDownloader;
+
+internal record DownloadConfig
+{
+   [YamlMember(Alias = "url")]
+   public string URL { get; set; } = null!;
+
+   [YamlMember(Alias = "file")]
+   public string File { get; set; } = null!;
+
+   [YamlMember(Alias = "sha1")]
+   public string? SHA1 { get; set; }
+
+   [YamlMember(Alias = "sha256")]
+   public string? SHA256 { get; set; }
+
+   [YamlMember(Alias = "overwrite")]
+   public bool? Overwrite { get; set; }
+}
--- CLIDownloader/Program.cs
+++ CLIDownloader/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System.CommandLine;
 
+using CLIDownloader;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
 var rootCommand = new RootCommand("Downloads multiple files from various sources in parallel");
 
+// Set up "download [--verbose] [--dry-run] [parallel-downloads=N] config.yml" command
 var verboseOption = new Option<bool>("--verbose");
 
 var dryRunOption = new Option<bool>("--dry-run");
@@ -11,19 +19,59 @@ var parallelDownloadsOption = new Option<int>
     description: "An option whose argument is parsed as an int.");
 
 var configArgument = new Argument<string>(
-   name: "configuration-file", "Configuration file (.yaml) which contains all necessary data you need for the download job");
+   name: "configuration-file", "Configuration file (.yaml) which contains all necessary data needed for the download job");
 
 var downloadCommand = new Command("download", "Downloads files from urls")
-{
-   verboseOption,
-   dryRunOption,
-   parallelDownloadsOption,
-   configArgument
-};
-downloadCommand.SetHandler((isVerbose, isDryRun, parallelDownloads, configFile)
-      => Console.WriteLine($"{isVerbose} ; {isDryRun} ; {parallelDownloads} ; {configFile}"),
+   {
+      verboseOption,
+      dryRunOption,
+      parallelDownloadsOption,
+      configArgument
+   };
+
+downloadCommand.SetHandler(
+   async (isVerbose, isDryRun, parallelDownloads, configFile) =>
+   {
+      await ExecuteDownloadCommand(isVerbose, isDryRun, parallelDownloads, configFile);
+   },
    verboseOption, dryRunOption, parallelDownloadsOption, configArgument);
 
 rootCommand.Add(downloadCommand);
 
+// Set up "validate [--verbose] config.yml" command
+var validateCommand = new Command("validate", "Validates downloaded files integrity against their respective checksums if available")
+   {
+      verboseOption,
+      configArgument
+   };
+
+rootCommand.Add(validateCommand);
 await rootCommand.InvokeAsync(args);
+
+static async Task ExecuteDownloadCommand(bool isVerbose, bool isDryRun, int parallelDownloads, string configFile)
+{
+   //The builder checks if the config file exists, and throws an exception accordingly
+   IHost host = Host.CreateDefaultBuilder()
+      .ConfigureAppConfiguration(builder =>
+      {
+         builder.AddYamlFile(configFile, optional: false);
+      })
+      .ConfigureServices(services =>
+      {
+         services.AddSingleton<HttpClient>();
+      })
+      .Build();
+
+   // Read configuration
+   var conf = host.Services.GetService<IConfiguration>()!;
+   if (parallelDownloads == 0)
+   {
+      parallelDownloads = conf.GetValue<int>("config:parallel_downloads");
+   }
+   var downloadDirectory = conf.GetValue<string>("config:download_dir")!;
+   var directory = Directory.CreateDirectory(conf.GetValue<string>("config:download_dir")!);
+
+   //var client = host.Services.GetService<HttpClient>()!;
+   var list = new List<DownloadConfig>();
+   conf.GetRequiredSection("downloads").Bind(list);
+}
